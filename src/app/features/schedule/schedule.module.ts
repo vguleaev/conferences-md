48e85d6b0f1dca4adad7263b897dce59ba14eaf6
Guleaev <gvladisl@techteam.com>
2019-03-05 15:46:09 +0200

    Switched to Angular Material lib

--- src/app/features/schedule/schedule.module.ts
+++ src/app/features/schedule/schedule.module.ts
@@ -8,9 +8,10 @@ import { SharedModule } from "src/app/shared/shared.module";
 import { BannerComponent } from './components/banner/banner.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { BubblesComponent } from './components/bubbles/bubbles.component';
+import { AngularMaterialModule } from './../../shared/modules/angular-material.module';
 
 @NgModule({
   declarations: [WelcomeComponent, BannerComponent, CalendarComponent, BubblesComponent],
-  imports: [CommonModule, ScheduleRoutingModule, MaterialModule, SharedModule]
+  imports: [CommonModule, ScheduleRoutingModule, MaterialModule, AngularMaterialModule, SharedModule]
 })
 export class ScheduleModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { MaterialModule } from "./modules/material.module";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { LoaderComponent } from './components/loader/loader.component';
+import { AngularMaterialModule } from './modules/angular-material.module';
 
 @NgModule({
   declarations: [NavbarComponent, FooterComponent, LoaderComponent],
-  imports: [CommonModule, MaterialModule],
-  exports: [NavbarComponent, FooterComponent, LoaderComponent]
+  imports: [CommonModule, MaterialModule, AngularMaterialModule],
+exports: [NavbarComponent, FooterComponent, LoaderComponent]
 })
 export class SharedModule {}
